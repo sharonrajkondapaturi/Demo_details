69dd9e37484f35c87b4e06c83d7bb37658946143
sharonrajkondapaturi <138500511+sharonrajkondapaturi@users.noreply.github.com>
2024-06-08 10:19:25 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const app = require("./server")
-
-const port = process.env.PORT || 4000
-const server = http.createServer(app)
-
-server.listen(port, ()=>{
-    console.log(`SERVER running ${port}`)
-})
